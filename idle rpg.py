1b5304935c6026383b4f72ea86639a7361daf453
Thomaz Leventhal <leventhalsan@gmail.com>
2026-06-21 20:18:42 -0300

    add: more colors and small tweaks

--- idle rpg.py
+++ idle rpg.py
@@ -17,7 +17,7 @@ def nome_equip(jogador, equiploja):
 def exibe_status(jogador, equiploja, levelup=False): # Função basica que mostra os status do jogador caso necessário
     print('-'*50)
     if levelup:
-        print('\033[32mSeus status aumentaram!\033[0m')
+        print('\033[92mSeus status aumentaram!\033[0m')
         for i, j in jogador.items():
             if i != "vida_atual" and i != "exp" and i != "nivel" and i != "nome" and i != "ouro" and i != 'pocao' and i != "equipamento":
                 time.sleep(0.2)
@@ -59,7 +59,7 @@ def curandeiro(jogador): # O curandeiro, onde se pode pagar ouro para obter cura
                  jogador['vida_atual'] == jogador['vida']
         else:
             try:
-                curarounao = int(input(f'O curandeiro diz:\n\033[36m"Para te curar completamente, cobrarei {custo_cura} de ouros, tudo bem?"\033[0m\n1- Sim, por favor\n2- Não, obrigado (Retornar a praça principal)\n'))
+                curarounao = int(input(f'O curandeiro diz:\n\033[36m"Para te curar completamente, cobrarei \033[33m{custo_cura} de ouro\033[0m, tudo bem?"\033[0m\n1- Sim, por favor\n2- Não, obrigado (Retornar a praça principal)\n'))
             except ValueError:
                 print('\033[31mDigite apenas números válidos!\033[0m')
                 return curandeiro(jogador)
@@ -112,6 +112,9 @@ def loja(jogador, iloja, equiploja): #loja onde o jogador pode comprar itens
             print('\033[36m"Ótima escolha, faça bom proveito..."\033[0m')
             jogador['ouro'] -= iloja[escolha-1]["preco"]
             if iloja[escolha-1]["nome"] == "Poção":
+                if jogador['pocao'] == True:
+                    print('Você não pode carregar mais de um desse item!')
+                    return loja(jogador, iloja, equiploja)
                 jogador['pocao'] = True
             
     if escolha < 1 or escolha > len(iloja)+2:
@@ -119,7 +122,7 @@ def loja(jogador, iloja, equiploja): #loja onde o jogador pode comprar itens
         return loja(jogador, iloja, equiploja)
     
 def arena(jogador, inimigos): # A arena, onde o usuario pode escolher com qual monstro lutar para conseguir ficar mais forte
-    print('Bem-vindo a arena!\ncom quem deseja lutar?')
+    print('\033[94mBem-vindo a arena!\ncom quem deseja lutar?/033[0m')
     
     for i in range(len(inimigos)):
         print(f'{i+1}- {inimigos[i]["nome"]} | Nível: {inimigos[i]["nivel"]}')
@@ -136,11 +139,11 @@ def arena(jogador, inimigos): # A arena, onde o usuario pode escolher com qual m
 
     
     elif escolha <= len(inimigos):    
-        print('Se prepare!')
+        print('\033[94mSe prepare!\033[0m')
         if luta(jogador, inimigos[escolha-1]):
-            print(f'\033[33mParabéns pela vitória! Talvez seja uma boa ideia se curar um pouco.\033[0m')
+            print(f'\033[94mParabéns pela vitória! Talvez seja uma boa ideia se curar um pouco.\033[0m')
         else:
-            print(f'\033[33mCaramba, Esse inimigo era muito forte! Talvez você devesse treinar mais\nNão esqueça de se curar, pois sua vida está em 0!\033[0m')
+            print(f'\033[94mCaramba, Esse inimigo era muito forte! Talvez você devesse treinar mais\nNão esqueça de se curar, pois sua vida está em 0!\033[0m')
         subiu_de_nivel(jogador)
     
 
@@ -153,7 +156,7 @@ def menu(jogador, inimigos, iloja, equiploja): # o menu principal do jogo, onde 
         return
     if escolha == 1:
         if jogador["vida_atual"] == 0:
-            print("Você está exausto demais para lutar na arena, tente se curar um pouco.")
+            print("\033[94mVocê está exausto demais para lutar na arena, tente se curar um pouco.\033[0m")
         else: arena(jogador, inimigos)
     elif escolha == 2: loja(jogador, iloja, equiploja)
     elif escolha == 3: curandeiro(jogador)
@@ -172,9 +175,8 @@ def subiu_de_nivel(jogador): #função chamada para verificar caso o jogador ten
     if jogador['exp'] >= barreira_de_xp:
         jogador['nivel'] += 1
         print('-'*50)
-        print(f'\033[32mSUBIU DE NÍVEL!\n{jogador["nivel"]-1} -> {jogador["nivel"]}\033[0m')
+        print(f'\033[92mSUBIU DE NÍVEL!\033[0m\n{jogador["nivel"]-1} -> {jogador["nivel"]}')
         print('Sua vida se regenerou ao máximo!')
-        print('-'*50)
         time.sleep(0.5)
         jogador['vida'] += 10
         jogador['vida_atual'] = jogador['vida']
@@ -193,29 +195,30 @@ def subiu_de_nivel(jogador): #função chamada para verificar caso o jogador ten
 def luta(jogador, inimigo): #função utilizada pra começar uma luta quando o jogador está na arena
     inimigo_atual = inimigo.copy()
     print('-'*50)
-    print(f'{jogador["nome"]} está contra {inimigo_atual["nome"]}')
+    print(f'\033[32m{jogador["nome"]}\033[0m está contra {inimigo_atual["nome"]}')
     print('-'*50)
 
     while jogador["vida_atual"] > 0 and inimigo_atual["vida_atual"] > 0:
         print('-'*50)
-        print(f'{jogador["nome"]}: {jogador["vida_atual"]} de vida | {inimigo_atual["nome"]}: {inimigo_atual["vida_atual"]} de vida')
+        if jogador['vida_atual'] >= jogador['vida'] * 0.10: print(f'{jogador["nome"]}: \033[32m{jogador["vida_atual"]} de vida\033[0m | {inimigo_atual["nome"]}: {inimigo_atual["vida_atual"]} de vida')
+        else: print(f'{jogador["nome"]}: \033[31m{jogador["vida_atual"]} de vida\033[0m | {inimigo_atual["nome"]}: {inimigo_atual["vida_atual"]} de vida')
         print('-'*50)
         time.sleep(1)
         
         if jogador['vida_atual'] <= jogador['vida'] * 0.25 and jogador['pocao'] == True:
             jogador['pocao'] = False
-            print(f'{jogador["nome"]} bebe sua poção, se regenerando por completo')
+            print(f'{jogador["nome"]} bebe sua poção, \033[32mse regenerando por completo\033[0m')
             jogador['vida_atual'] = jogador['vida']
 
         #dano do jogador
         dano_causado_jogador = random.randint(jogador["dano_min"], jogador["dano_max"]) - inimigo_atual["defesa"]
         if dano_causado_jogador <= 0: dano_causado_jogador = 1
-        print(f'{jogador["nome"]} ataca e atinge o(a) {inimigo_atual["nome"]}, causando {dano_causado_jogador} de dano...')
+        print(f'{jogador["nome"]} \033[31mataca e atinge o(a)\033[0m {inimigo_atual["nome"]}, causando \033[31m{dano_causado_jogador}\033[0m de dano...')
         inimigo_atual["vida_atual"] -= dano_causado_jogador
         time.sleep(1)
 
         if inimigo_atual["vida_atual"] <= 0: #checagem de morte
-            print(f'Você venceu!\nGanhando {inimigo_atual["exp_dado"]} de experiência e {inimigo_atual["ouro_dado"]} de ouro.')
+            print(f'\033[92mVocê venceu!\033[0m\nGanhando \033[96m{inimigo_atual["exp_dado"]} de experiência\033[0m e \033[33m{inimigo_atual["ouro_dado"]} de ouro.\033[0m')
             jogador["exp"] += inimigo_atual["exp_dado"]
             jogador["ouro"] += inimigo_atual["ouro_dado"]
             return True
@@ -223,14 +226,14 @@ def luta(jogador, inimigo): #função utilizada pra começar uma luta quando o j
         #dano do inimigo
         dano_causado_inimigo = random.randint(inimigo_atual["dano_min"], inimigo_atual["dano_max"]) - jogador["defesa"]
         if dano_causado_inimigo <= 0: dano_causado_inimigo = 1
-        print(f'{inimigo_atual["nome"]} ataca e atinge o(a) {jogador["nome"]}, causando {dano_causado_inimigo} de dano...')
+        print(f'{inimigo_atual["nome"]} \033[31mataca e atinge o(a)\033[0m {jogador["nome"]}, causando \033[31m{dano_causado_inimigo}\033[0m de dano...')
         jogador["vida_atual"] -= dano_causado_inimigo
         if jogador["vida_atual"] < 0: jogador["vida_atual"] = 0
         time.sleep(1)
 
     else:
         if inimigo["vida_atual"] <= 0 and jogador["vida_atual"] > 0:
-            print(f'Você venceu!\nGanhando {inimigo_atual["exp_dado"]} de experiência e {inimigo_atual["ouro_dado"]} de ouro.')
+            print(f'\033[92mVocê venceu!\033[0m\nGanhando \033[96m{inimigo_atual["exp_dado"]} de experiência\033[0m e \033[33m{inimigo_atual["ouro_dado"]} de ouro.\033[0m')
             jogador["exp"] += inimigo_atual["exp_dado"]
             jogador["ouro"] += inimigo_atual["ouro_dado"]
             time.sleep(1)
@@ -268,6 +271,7 @@ def main():
     "exp_dado": 5,
     "ouro_dado": 10,
     "nivel": 1},
+    
     {"nome": "Goblin",
     "vida": 160,
     "vida_atual": 160,
@@ -277,6 +281,7 @@ def main():
     "exp_dado": 9,
     "ouro_dado": 20,
     "nivel": 5},
+    
     {"nome": "Orc",
     "vida": 220,
     "vida_atual": 220,
@@ -286,6 +291,7 @@ def main():
     "exp_dado": 15,
     "ouro_dado": 30,
     "nivel": 10},
+    
     {"nome": "Gigante",
     "vida": 300,
     "vida_atual": 300,
@@ -295,6 +301,7 @@ def main():
     "exp_dado": 25,
     "ouro_dado": 45,
     "nivel": 15},
+    
     {"nome": "Dragão",
     "vida": 500,
     "vida_atual": 500,
@@ -303,7 +310,7 @@ def main():
     "defesa": 15,
     "exp_dado": 50,
     "ouro_dado": 70,
-    "nivel": '?'},
+    "nivel": '∞'},
     ]
 
     iloja = [ #Lista de itens na loja que não são equipamentos
